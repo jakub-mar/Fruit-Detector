f6dcbb5a493a5bbcf95b81271788d2070a2263bf
Jakub Marciniak <jamarciniak@pop-os.localdomain>
2022-02-03 23:11:44 +0100

    Working detector

--- detect_fruits.py
+++ detect_fruits.py
@@ -7,6 +7,119 @@ from tqdm import tqdm
 
 from typing import Dict
 
+import numpy as np
+from pykuwahara import kuwahara
+
+
+def ReadyImage(source, interpolation_type, scale):
+    image = cv2.imread(source, cv2.IMREAD_UNCHANGED)
+    image_resized = cv2.resize(
+        image, (int(image.shape[0]/scale), int(image.shape[1]/scale)), interpolation=interpolation_type)
+    return image_resized
+
+
+def ImageFiltering(if_image):
+    image_kuwahara = kuwahara(if_image, method='mean', radius=15)
+    image_median = cv2.medianBlur(image_kuwahara, 15)
+    image_hsv = cv2.cvtColor(image_median, cv2.COLOR_BGR2HSV)
+    return image_hsv
+
+
+def FruitsDetection(filtered_image):
+    fruits_l = (0, 94, 0)
+    fruits_h = (255, 255, 255)
+
+    fruits_mask = cv2.inRange(filtered_image, fruits_l, fruits_h)
+    opening = cv2.morphologyEx(
+        fruits_mask, cv2.MORPH_OPEN, kernel=cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2)))
+    closing = cv2.morphologyEx(
+        opening, cv2.MORPH_CLOSE, kernel=cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2)))
+
+    return closing
+
+
+def FindBanana(filtered_image, fruits_detected, area, image_to_contours):
+    banana_counter = 0
+
+    banana_l = (20, 59, 180)
+    banana_h = (69, 255, 255)
+    banana_mask = cv2.inRange(filtered_image, banana_l, banana_h)
+    banana_mask = cv2.morphologyEx(
+        banana_mask, cv2.MORPH_CLOSE, np.ones((41, 41), np.uint8))
+    banana_mask = cv2.morphologyEx(
+        banana_mask, cv2.MORPH_OPEN, np.ones((41, 41), np.uint8))
+
+    contours, hierarchy = cv2.findContours(
+        banana_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        if cv2.contourArea(cnt) >= area:
+            banana_counter += 1
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(image_to_contours, (x, y),
+                          (x+w, y+h), (255, 0, 0), 2)
+            cv2.putText(image_to_contours, "BANANA", (x, y-10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+    return banana_counter, banana_mask
+
+
+def FindApple(filtered_image, fruits_detected, area, image_to_contours):
+    apple_counter = 0
+
+    apple_l = (0, 79, 17)
+    apple_h = (18, 209, 220)
+    apple_mask1 = cv2.inRange(filtered_image, apple_l, apple_h)
+    apple_mask1 = cv2.morphologyEx(
+        apple_mask1, cv2.MORPH_CLOSE, np.ones((41, 41), np.uint8))
+    apple_mask1 = cv2.morphologyEx(
+        apple_mask1, cv2.MORPH_OPEN, np.ones((41, 41), np.uint8))
+    apple_mask1 = cv2.bitwise_and(apple_mask1, fruits_detected)
+
+    apple_l2 = (80, 76, 0)
+    apple_h2 = (179, 255, 255)
+    apple_mask2 = cv2.inRange(filtered_image, apple_l, apple_h)
+    apple_mask2 = cv2.morphologyEx(
+        apple_mask2, cv2.MORPH_CLOSE, np.ones((41, 41), np.uint8))
+    apple_mask2 = cv2.morphologyEx(
+        apple_mask2, cv2.MORPH_OPEN, np.ones((41, 41), np.uint8))
+
+    apple_mask = cv2.bitwise_or(apple_mask1, apple_mask2)
+
+    contours, hierarchy = cv2.findContours(
+        apple_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        if cv2.contourArea(cnt) > area:
+            apple_counter += 1
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(image_to_contours, (x, y),
+                          (x+w, y+h), (0, 255, 0), 2)
+            cv2.putText(image_to_contours, "APPLE", (x, y-10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+    return apple_counter, apple_mask
+
+
+def FindOrange(filtered_image, fruits_detected, area, image_to_contours):
+    orange_counter = 0
+    orange_l = (11, 65, 152)
+    orange_h = (19, 255, 255)
+    orange_mask = cv2.inRange(filtered_image, orange_l, orange_h)
+    orange_mask = cv2.morphologyEx(
+        orange_mask, cv2.MORPH_CLOSE, np.ones((41, 41), np.uint8))
+    orange_mask = cv2.morphologyEx(
+        orange_mask, cv2.MORPH_OPEN, np.ones((41, 41), np.uint8))
+    orange_mask = cv2.bitwise_and(orange_mask, fruits_detected)
+
+    contours, hierarchy = cv2.findContours(
+        orange_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        if cv2.contourArea(cnt) > area:
+            orange_counter += 1
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(image_to_contours, (x, y),
+                          (x+w, y+h), (0, 0, 255), 2)
+            cv2.putText(image_to_contours, "ORANGE", (x, y-10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    return orange_counter, orange_mask
+
 
 def detect_fruits(img_path: str) -> Dict[str, int]:
     """Fruit detection function, to implement.
@@ -21,13 +134,17 @@ def detect_fruits(img_path: str) -> Dict[str, int]:
     Dict[str, int]
         Dictionary with quantity of each fruit.
     """
-    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 
-    #TODO: Implement detection method.
-    
-    apple = 0
-    banana = 0
-    orange = 0
+    image_r = ReadyImage(img_path, cv2.INTER_CUBIC, 2)
+    image_fil = ImageFiltering(image_r)
+    fruits_detected = FruitsDetection(image_fil)
+
+    area = image_r.shape[1]*image_r.shape[0]*0.005
+    banana, ban_mask = FindBanana(image_fil, fruits_detected, area, image_r)
+    fruits_detected = fruits_detected-ban_mask
+    apple, app_mask = FindApple(image_fil, fruits_detected, area, image_r)
+    fruits_detected = fruits_detected - app_mask
+    orange, oran_mask = FindOrange(image_fil, fruits_detected, area, image_r)
 
     return {'apple': apple, 'banana': banana, 'orange': orange}
 
@@ -47,7 +164,7 @@ def main(data_path, output_file_path):
         filename = img_path.split('/')[-1]
 
         results[filename] = fruits
-    
+
     with open(output_file_path, 'w') as ofp:
         json.dump(results, ofp)
 
